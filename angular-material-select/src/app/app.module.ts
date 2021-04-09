39765df299b7dd49995eba5aceb41e1a2ba084fd
Boldizsar Norbert Benjamin <boldizsarnorbertb@gmail.com>
2021-04-09 19:59:18 +0300

    Adding example project.

--- angular-material-select/src/app/app.module.ts
+++ angular-material-select/src/app/app.module.ts
@@ -4,13 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NextPageComponent } from './next-page/next-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+
+const routes: Routes = [
+  { path: 'home', component: MainPageComponent },
+  { path: 'nextPage', component: NextPageComponent },
+  { path: '**', redirectTo: '/home' }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NextPageComponent,
+    MainPageComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSelectModule,
+    BrowserAnimationsModule, 
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
